ae30b319d7ff3ddc8468891c937cd1fbbb86d1a0
Amar Hodzic <amar_hodzic@hotmail.com>
2021-02-23 19:01:11 +0100

    refactor: Change so the if-statement not only look that one of the arguments is a string, but also that it is a valid string char. Also changed the return statment to raise a ValueError instead of string #2

--- algorithms/maths/pythagoras.py
+++ algorithms/maths/pythagoras.py
@@ -3,14 +3,11 @@ input two of the three side in right angled triangle and return the third. use "
 """
 
 def pythagoras(opposite,adjacent,hypotenuse):
-    try:
-        if opposite == str("?"):
-            return ("Opposite = " + str(((hypotenuse**2) - (adjacent**2))**0.5))
-        elif adjacent == str("?"):
-            return ("Adjacent = " + str(((hypotenuse**2) - (opposite**2))**0.5))
-        elif hypotenuse == str("?"):
-            return ("Hypotenuse = " + str(((opposite**2) + (adjacent**2))**0.5))
-        else:
-            return "You already know the answer!"
-    except:
+    if isinstance(opposite, str) and opposite == "?":
+        return ("Opposite = " + str(((hypotenuse**2) - (adjacent**2))**0.5))
+    elif isinstance(adjacent, str) and adjacent == "?":
+        return ("Adjacent = " + str(((hypotenuse**2) - (opposite**2))**0.5))
+    elif isinstance(hypotenuse, str) and hypotenuse == "?":
+        return ("Hypotenuse = " + str(((opposite**2) + (adjacent**2))**0.5))
+    else:
         raise ValueError("invalid argument were given.")
