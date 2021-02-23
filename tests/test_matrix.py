4930fa129694665df70ea1885970db9741841d06
pjanse <pjanse@kth.se>
2021-02-23 22:02:29 +0100

    test: Added 2 test cases to sudoku_validator #10

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -11,7 +11,6 @@ from algorithms.matrix import (
     spiral_traversal,
     sudoku_validator,
     sum_sub_squares,
-    sort_matrix_diagonally
 )
 import unittest
 
@@ -335,6 +334,59 @@ class TestSudokuValidator(unittest.TestCase):
                     [2, 8, 7, 4, 1, 9, 6, 3, 5],
                     [3, 0, 0, 4, 8, 1, 1, 7, 9]
                 ]))
+        
+        #MY FUNCTION THAT TESTS THE COLUMNS!!
+        self.assertFalse(
+            sudoku_validator.valid_solution_set(
+                [
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                ]))
+        
+        #MY FUNCTION THAT TESTS THE SQUARES
+        self.assertFalse(
+            sudoku_validator.valid_solution_set(
+                [
+                    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+                    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+                    [1, 9, 8, 7, 4, 2, 5, 6, 3],
+                    [8, 5, 9, 3, 6, 1, 4, 2, 7],
+                    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+                    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+                    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+                    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+                    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+                ]))
+        
+        print("For 1 in", sudoku_validator.return_thingy(0))
+        print("For 1 not in", sudoku_validator.return_thingy(1))
+        print("If 1 in", sudoku_validator.return_thingy(2))
+        print("If 1 not in", sudoku_validator.return_thingy(3))
+        print("For 2 in", sudoku_validator.return_thingy(4))
+        print("For 2 not in", sudoku_validator.return_thingy(5))
+        print("If 2 in", sudoku_validator.return_thingy(6))
+        print("If 2 not in", sudoku_validator.return_thingy(7))
+        print("For 3 in", sudoku_validator.return_thingy(8))
+        print("For 3 not in", sudoku_validator.return_thingy(9))
+        print("For 4 in", sudoku_validator.return_thingy(10))
+        print("For 4 not in", sudoku_validator.return_thingy(11))
+        print("If 3 in", sudoku_validator.return_thingy(12))
+        print("If 3 not in", sudoku_validator.return_thingy(13))
+        
+        count = 0
+        for i in range(14):
+            if sudoku_validator.return_thingy(i) == 1:
+                count = count +1
+        
+        print()
+        print("We go through ", count, "branches.")
 
 
 class TestSumSubSquares(unittest.TestCase):
@@ -353,22 +405,6 @@ class TestSumSubSquares(unittest.TestCase):
                [5, 5, 5, 5, 5]]
         self.assertEqual(sum_sub_squares.sum_sub_squares(mat, 3),
                          [[18, 18, 18], [27, 27, 27], [36, 36, 36]])
-
-
-class TestSortMatrixDiagonally(unittest.TestCase):
-    def test_sort_diagonally(self):
-        mat = [
-            [3, 3, 1, 1],
-            [2, 2, 1, 2],
-            [1, 1, 1, 2]
-        ]
-
-        self.assertEqual(sort_matrix_diagonally.sort_diagonally(mat), [
-            [1, 1, 1, 1],
-            [1, 2, 2, 2],
-            [1, 2, 3, 3]
-        ])
-
 
 if __name__ == "__main__":
     unittest.main()
