466078cf6ac4b19dbd64853f197acbe5e2fd7963
pjanse <pjanse@kth.se>
2021-02-24 12:16:46 +0100

    tests: Added 4 more tests #11
    Closes #11

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -364,6 +364,64 @@ class TestSudokuValidator(unittest.TestCase):
                     [2, 8, 7, 4, 1, 9, 6, 3, 5],
                     [3, 4, 5, 2, 8, 6, 1, 7, 9]
                 ]))
+        #MY FUNCTION THAT TESTS THE COLUMNS!!
+        self.assertFalse(
+            sudoku_validator.valid_solution_hashtable(
+                [
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                ]))
+        
+        #MY FUNCTION THAT TESTS THE SQUARES
+        self.assertFalse(
+            sudoku_validator.valid_solution_hashtable(
+                [
+                    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+                    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+                    [1, 9, 8, 7, 4, 2, 5, 6, 3],
+                    [8, 5, 9, 3, 6, 1, 4, 2, 7],
+                    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+                    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+                    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+                    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+                    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+                ]))
+        #MY FUNCTION THAT TESTS THE COLUMNS!!
+        self.assertFalse(
+            sudoku_validator.valid_solution(
+                [
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+                ]))
+        
+        #MY FUNCTION THAT TESTS THE SQUARES
+        self.assertFalse(
+            sudoku_validator.valid_solution(
+                [
+                    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+                    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+                    [1, 9, 8, 7, 4, 2, 5, 6, 3],
+                    [8, 5, 9, 3, 6, 1, 4, 2, 7],
+                    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+                    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+                    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+                    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+                    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+                ]))
         
         print("For 1 in", sudoku_validator.return_thingy(0))
         print("For 1 not in", sudoku_validator.return_thingy(1))
